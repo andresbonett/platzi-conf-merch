bf17bf7a660dcfec156d0ce57cd24d52413b89b5
Andres Bonett <andresbonettm@gmail.com>
2020-12-22 16:52:01 -0500

    agregando componente de google maps

---
+++ src/components/Map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+export default function Map() {
+  const mapStyles = {
+    height: '50vh',
+    width: '100%',
+  };
+
+  const defaultCenter = {
+    lat: 19.4267261,
+    lng: -99.1718706,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw">
+      <GoogleMap mapContainerStyle={mapStyles} zoom={17} center={defaultCenter}>
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  );
+}
