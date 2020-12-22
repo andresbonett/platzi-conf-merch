013c2cdfb357201643a1d032f760d8563bdb91cc
Andres Bonett <andresbonettm@gmail.com>
2020-12-22 16:59:40 -0500

    agregando componente Map a Success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,12 +1,20 @@
 import '../styles/components/Success.css';
+import { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
 
 export default function Succsses() {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+
   return (
     <div className="Succes">
       <div className="Success-content">
-        <h2>Nombre, Gracias por tu compra</h2>
+        <h2>{buyer.name}, Gracias por tu compra</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-        <div className="Success-map">Google Maps</div>
+        <div className="Success-map">
+          <Map />
+        </div>
       </div>
     </div>
   );
